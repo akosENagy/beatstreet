2d3de629e478a7cfd4af7572468e6ffa41d4b7ad
akosENagy <akos.e.nagy@gmail.com>
2018-01-17 10:23:15 +0100

    Add pom and main class

---
+++ src/main/java/com/cybersoft/beatstreet/BeatstreetApp.java
@@ -0,0 +1,12 @@
+package com.cybersoft.beatstreet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatstreetApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeatstreetApp.class, args);
+    }
+}
