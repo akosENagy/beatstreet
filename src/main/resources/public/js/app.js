c576f59a355c33f3714b814726e58a969f5dd387
akosENagy <akos.e.nagy@gmail.com>
2018-02-15 11:15:59 +0100

    Add player-body songs basic design

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -16,11 +16,36 @@ var addGenreEventHandlers = function() {
 var loadGenre = function() {
     var genre = this.children[0].innerHTML;
     $.get("http://localhost:60001/api/genres/" + genre, function(data) {
+        var playerBody = document.getElementById("player-main-body");
+        while (playerBody.firstChild) {
+            playerBody.removeChild(playerBody.firstChild);
+        }
 
-        alert(data);
-        // data == list of songs from that genre (JSON array).
-        // TODO : load songs into main window.
+        for (let song of data) {
+            buildSongElement(song);
+        }
     });
+};
+
+var buildSongElement = function(song) {
+    var playerBody = document.getElementById("player-main-body");
+
+    var songTitle = document.createElement("h3");
+    songTitle.className += "player-body-song-title";
+    songTitle.innerHTML = song["title"];
+
+    //title, id, path, princeincents, owner, lengthinseconds
+
+    var songOwner = document.createElement("p");
+    songOwner.className += "player-body-song-owner";
+    songOwner.innerHTML = song["owner"]["name"];
+
+    var songDiv = document.createElement("div");
+    songDiv.className += "playerbody-song";
+
+    songDiv.appendChild(songTitle);
+    songDiv.appendChild(songOwner);
+    playerBody.appendChild(songDiv);
 };
 
 $("document").ready(main);
