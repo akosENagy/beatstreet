facaf013e5a9bd53406b6cd266185904cc7978f3
akosENagy <akos.e.nagy@gmail.com>
2018-03-27 11:19:56 +0200

    Add javascript password confirmation checking

--- src/main/resources/public/js/dom.js
+++ src/main/resources/public/js/dom.js
@@ -10,5 +10,19 @@ app.dom = {
 				$('.navbar').fadeIn(300);
 			}
 		});
+	},
+
+	checkPasswords: function() {
+		let pass = document.getElementById("password").value;
+		let confirm = document.getElementById("pw-confirm").value;
+
+        if (pass !== confirm) {
+            alert("Passwords do not match!");
+            document.getElementById("password").style.borderColor = "#E34234";
+            document.getElementById("pw-confirm").style.borderColor = "#E34234";
+            return false;
+        }
+
+        return true;
 	}
-}
+};
